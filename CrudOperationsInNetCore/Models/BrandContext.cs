e9373a74469e5c0a73dd0860b02268bb9a20faa1
Gayathri <31729383+gayathripothieni@users.noreply.github.com>
2024-08-24 23:01:25 -0400

    Initial commit

---
+++ CrudOperationsInNetCore/Models/BrandContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperationsInNetCore.Models
+{
+    public class BrandContext : DbContext
+    {
+        public BrandContext( DbContextOptions<BrandContext> options) : base(options)
+        {
+            
+
+        }
+
+       public DbSet<Brand> Brands { get; set; }
+    }
+}
